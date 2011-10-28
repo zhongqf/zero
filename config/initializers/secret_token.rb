9f67d5a57cc9f8ce8ed6d03b4b34aa3e81c0b4c6
zhongqf <zhongqf@gmail.com>
2011-10-29 00:36:18 +0800

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Plusone::Application.config.secret_token = 'd92cdcaa71696d4cab77af868750b39081659cc903ada1777f8d6f23225d811e0528e900fe3dbef77e46348b4c4f90340527407e24a476d507b6a7b54b327af4'
